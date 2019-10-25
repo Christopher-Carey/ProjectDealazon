fb723648b63b69d27adddac5332a2de8e7a9e61a
Christopher-Carey <ccarey40@gmail.com>
2019-10-25 11:10:40 -0500

    clean up

--- apps/deal_app/static/deal_app/js/ajax.js
+++ apps/deal_app/static/deal_app/js/ajax.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-    $('#click_btn').click(function(){
-        console.log('clicked buttons')
-        $.ajax({
-            method:"GET",
-            url:"/deals/update_price"
-        })
-        .done(function(){
-            console.log("We did it!!")
-        })
-    })
     setTimeout(function(){ 
         $.ajax({
             method:"GET",
